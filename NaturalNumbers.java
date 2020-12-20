5190f73360b9ec6182f1255adb7c988783dcaea0
Jakub <jakubkrys@gmail.com>
2020-12-20 23:51:58 +0100

    #7 Działanie na liczbach naturalnych

---
+++ NaturalNumbers.java
@@ -0,0 +1,42 @@
+class NaturalNumbers{
+	public static void main(String[] args){
+		int n1 = 5;
+		int n2 = 4;
+		int n3 = 3;
+		int n4 = 2;
+		int n5 = 1;
+		
+		System.out.println("--------------------------------");
+		if(n1*2+4 >= 8){
+			System.out.println("Nie");
+		} else {
+			System.out.println("Tak");
+		}
+		System.out.println("--------------------------------");
+		if(n2*2+4 >= 8){
+			System.out.println("Nie");
+		} else {
+			System.out.println("Tak");
+		}
+		System.out.println("--------------------------------");
+		if(n3*2+4 >= 8){
+			System.out.println("Nie");
+		} else {
+			System.out.println("Tak");
+		}
+		System.out.println("--------------------------------");
+		if(n4*2+4 >= 8){
+			System.out.println("Nie");
+		} else {
+			System.out.println("Tak");
+		}
+		System.out.println("--------------------------------");
+		if(n5*2+4 >= 8){
+			System.out.println("Nie");
+		} else {
+			System.out.println("Tak");
+		}
+		System.out.println("--------------------------------");
+	}
+
+}
